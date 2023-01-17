f5d3d269efe8fe4bbac6fd1ebd6915ffb00ab17a
Sanyi81 <skyharts2002@gmail.com>
2023-01-17 19:13:50 +0200

    mongo twilio

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -1,5 +1,8 @@
 const oauthService = require("../service/oauth.service");
 const emailService = require("../service/email.service");
+const smsService = require("../service/sms.service");
+const { smsActionTypeEnum } = require("../enum");
+const smsTemplate = require("../helper/sms-template.helper");
 const ActionToken = require("../dataBase/ActionToken");
 const OAuth = require("../dataBase/OAuth");
 const OldPassword = require('../dataBase/OldPassword')
@@ -13,7 +16,10 @@ module.exports = {
         try {
             const { user, body } = req;
 
-            await emailService.sendEmail(user.email, WELCOME, { userName: user.name });
+            await Promise.allSettled([
+                emailService.sendEmail(user.email, WELCOME, { userName: user.name }),
+                smsService.sendSms(smsTemplate[smsActionTypeEnum.WELCOME]({ name: user.name }), user.phone)
+            ]);
 
             await user.comparePasswords(body.password);
 
